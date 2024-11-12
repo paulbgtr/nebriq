d5c662ab6fe55f5c2c560a98fa07c9db847f2518
paulbg <paulbg@hey.com>
2024-11-12 16:51:18 +0200

    chore: note-list update
    - pass created_at to note component
    - handle empty state
    - handle loading state

--- src/modules/notes/features/all-notes/components/note-list.tsx
+++ src/modules/notes/features/all-notes/components/note-list.tsx
@@ -3,13 +3,16 @@
 import { DetailedNote } from "./detailed-note";
 import { Note } from "./note";
 import { useNotes } from "@/hooks/useNotes";
+import { Spinner } from "@/components/spinner";
 
 export const NoteList = () => {
   const { getNotesQuery } = useNotes();
 
-  const { data: notes } = getNotesQuery;
+  const { data: notes, isPending } = getNotesQuery;
 
-  const createdAt = new Date(notes?.[0].created_at);
+  if (!notes) return null;
+
+  if (isPending) return <Spinner size="md" />;
 
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 p-4">
@@ -19,12 +22,12 @@ export const NoteList = () => {
           key={note.id}
           title={note.title}
           content={note.content}
-          createdAt={createdAt}
+          createdAt={note.created_at}
         >
           <Note
             title={note.title}
             content={note.content}
-            createdAt={createdAt}
+            createdAt={note.created_at}
           />
         </DetailedNote>
       ))}
