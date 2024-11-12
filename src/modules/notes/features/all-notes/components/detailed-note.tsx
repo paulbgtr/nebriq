b3df4fc76c767443e2d619ed388a0ca5abcabbfd
paulbg <paulbg@hey.com>
2024-11-12 16:22:37 +0200

    feat: implement note title update

--- src/modules/notes/features/all-notes/components/detailed-note.tsx
+++ src/modules/notes/features/all-notes/components/detailed-note.tsx
@@ -1,43 +1,83 @@
-import { Button } from "@/components/ui/button";
 import {
   Dialog,
   DialogContent,
-  DialogDescription,
-  DialogFooter,
-  DialogHeader,
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
+import { useEditor, EditorContent } from "@tiptap/react";
+import StarterKit from "@tiptap/starter-kit";
+import Placeholder from "@tiptap/extension-placeholder";
+import { useNotes } from "@/hooks/useNotes";
+import { useState } from "react";
 
 type DetailedNoteProps = {
   children: React.ReactNode;
+  id: string;
   title: string;
   content: string;
   createdAt: Date;
 };
 
 export const DetailedNote = ({
+  id,
   children,
-  title,
+  title: initialTitle,
   content,
   createdAt,
 }: DetailedNoteProps) => {
+  const [title, setTitle] = useState(initialTitle);
+
+  const editor = useEditor({
+    immediatelyRender: false,
+    extensions: [StarterKit, Placeholder.configure()],
+    editorProps: {
+      attributes: {
+        class: "prose prose-slate focus:outline-none",
+      },
+    },
+    content,
+  });
+
+  const { updateNoteMutation } = useNotes();
+
+  const handleTitleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const newTitle = e.target.value;
+    setTitle(newTitle);
+    console.log("Updating title to:", newTitle);
+
+    updateNoteMutation.mutate({
+      id,
+      title: newTitle,
+      content,
+      created_at: createdAt,
+    });
+  };
+
   return (
     <Dialog>
       <DialogTrigger asChild>
         <div>{children}</div>
       </DialogTrigger>
-      <DialogContent className="sm:max-w-[425px]">
-        <DialogHeader>
-          <DialogTitle>{title}</DialogTitle>
-          <DialogDescription>{content}</DialogDescription>
-          <div className="text-sm text-muted-foreground">
-            {createdAt.toLocaleDateString()}
+      <DialogContent className="sm:max-w-[90vw] sm:max-h-[90vh] w-[90vw] h-[90vh]">
+        <div className="grid items-center px-12">
+          <div className="flex flex-col space-y-0 gap-0">
+            <DialogTitle>
+              <input
+                type="text"
+                value={title}
+                className="focus:outline-none text-xl font-semibold"
+                onChange={handleTitleChange}
+              />
+            </DialogTitle>
+            <EditorContent
+              editor={editor}
+              className="mt-4 [&_.ProseMirror]:min-h-[200px]"
+            />
+            <div className="text-sm text-muted-foreground/60 mt-4">
+              {createdAt.toLocaleDateString()}
+            </div>
           </div>
-        </DialogHeader>
-        <DialogFooter>
-          <Button type="submit">Save changes</Button>
-        </DialogFooter>
+        </div>
       </DialogContent>
     </Dialog>
   );
