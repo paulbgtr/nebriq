dbfed399547d610190edc88b987fc87bcf3cb286
paulbg <paulbg@hey.com>
2024-11-12 14:39:14 +0200

    feat: implement notes server actions

---
+++ src/app/actions/notes.ts
@@ -0,0 +1,35 @@
+"use server";
+
+import { createClient } from "@/lib/supabase";
+import { type Note } from "@/types/note";
+
+export const getNotes = async () => {
+  const supabase = await createClient();
+  const { data: notes } = await supabase.from("notes").select("*");
+  console.log(notes);
+  return notes;
+};
+
+export const createNote = async (note: Note) => {
+  const supabase = await createClient();
+  const { data: newNote } = await supabase.from("notes").insert(note).select();
+  return newNote;
+};
+
+export const updateNote = async (note: Note) => {
+  const supabase = await createClient();
+  const { data: updatedNote } = await supabase
+    .from("notes")
+    .update(note)
+    .select();
+  return updatedNote;
+};
+
+export const deleteNote = async (id: string) => {
+  const supabase = await createClient();
+  const { data: deletedNote } = await supabase
+    .from("notes")
+    .delete()
+    .eq("id", id);
+  return deletedNote;
+};
