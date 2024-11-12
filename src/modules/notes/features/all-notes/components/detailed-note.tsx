904db88d7be804246cd58798174dee22f9f5d555
paulbg <paulbg@hey.com>
2024-11-12 13:46:58 +0200

    feat: implement detailed note component
    This component is used to display a note in a dialog.

---
+++ src/modules/notes/features/all-notes/components/detailed-note.tsx
@@ -0,0 +1,42 @@
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+
+type DetailedNoteProps = {
+  children: React.ReactNode;
+  title: string;
+  content: string;
+  createdAt: Date;
+};
+
+export const DetailedNote = ({
+  children,
+  title,
+  content,
+  createdAt,
+}: DetailedNoteProps) => {
+  return (
+    <Dialog>
+      <DialogTrigger asChild>{children}</DialogTrigger>
+      <DialogContent className="sm:max-w-[425px]">
+        <DialogHeader>
+          <DialogTitle>{title}</DialogTitle>
+          <DialogDescription>{content}</DialogDescription>
+          <div className="text-sm text-muted-foreground">
+            {createdAt.toLocaleDateString()}
+          </div>
+        </DialogHeader>
+        <DialogFooter>
+          <Button type="submit">Save changes</Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+};
