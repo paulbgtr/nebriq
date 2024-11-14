3b80208a5d8142c6c6fc2a1bf02f4fab989a4008
paulbg <paulbg@hey.com>
2024-11-14 16:51:37 +0200

    chore: sort notes by created_at

--- src/modules/notes/features/all-notes/components/note-list.tsx
+++ src/modules/notes/features/all-notes/components/note-list.tsx
@@ -29,9 +29,14 @@ export const NoteList = () => {
     );
   }
 
+  const sortedNotes = [...notes].sort(
+    (a, b) =>
+      new Date(b.created_at).getTime() - new Date(a.created_at).getTime()
+  );
+
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 p-4">
-      {notes.map((note) => (
+      {sortedNotes.map((note) => (
         <DetailedNote
           id={note.id}
           key={note.id}
