163282122e1eb2f3d7240bb9b0d1ada6f56b80ce
paulbg <paulbg@hey.com>
2024-11-16 14:48:46 +0200

    feat: implement logout

--- src/app/actions/auth.ts
+++ src/app/actions/auth.ts
@@ -45,3 +45,15 @@ export async function signup(formData: FormData) {
   revalidatePath("/", "layout");
   redirect("/");
 }
+
+export async function logout() {
+  const supabase = await createClient();
+  const { error } = await supabase.auth.signOut();
+
+  if (error) {
+    redirect("/error");
+  }
+
+  revalidatePath("/", "layout");
+  redirect("/");
+}
