424a504e57e886fc1b28121d2c05a7d743f3128b
paulbg <paulbg@hey.com>
2024-11-20 19:35:52 +0200

    feat: tfidf by tags

--- src/app/actions/search/tfidf.ts
+++ src/app/actions/search/tfidf.ts
@@ -10,19 +10,26 @@ export const searchUsingTFIDF = async (query: string, notes: Note[]) => {
   const TfIdf = natural.TfIdf;
   const tfidf = new TfIdf();
   const titleTfidf = new TfIdf();
+  const tagsTfidf = new TfIdf();
 
   notes.forEach((note) => {
     if (!note?.content) return;
     tfidf.addDocument(note.content);
     titleTfidf.addDocument(note.title || "");
+    const tagsString = note.tags?.join(" ") || "";
+    tagsTfidf.addDocument(tagsString);
   });
 
   const contentScores = tfidf.tfidfs(query);
   const titleScores = titleTfidf.tfidfs(query);
+  const tagScores = tagsTfidf.tfidfs(query);
 
   const results = notes.map((note, index) => ({
     note,
-    score: contentScores[index] + titleScores[index] * 2, // Give more weight to title matches
+    score:
+      contentScores[index] +
+      titleScores[index] * 2 + // Give more weight to title matches
+      tagScores[index] * 3, // Give even more weight to tag matches
   })) satisfies TFIDFResult[] | null;
 
   const filteredResults = results
