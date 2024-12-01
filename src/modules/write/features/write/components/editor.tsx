ff9f94bf46fd77dd3d13d9b5046bdea6b9adec75
paulbg <paulbg@hey.com>
2024-12-01 10:35:37 +0200

    feat: add indicator of unsaved changes

--- src/modules/write/features/write/components/editor.tsx
+++ src/modules/write/features/write/components/editor.tsx
@@ -1,6 +1,7 @@
 import { EditorContent } from "@tiptap/react";
 import { useNotes } from "@/hooks/use-notes";
 import { Editor as TiptapEditor } from "@tiptap/react";
+import { Check } from "lucide-react";
 
 type EditorProps = {
   id: string;
@@ -21,21 +22,33 @@ export const Editor = ({
 
   return (
     <div className="flex flex-col h-full gap-4">
-      <input
-        type="text"
-        value={title}
-        onChange={(e) => {
-          setTitle(e.target.value);
-          updateNoteMutation.mutate({
-            id,
-            title: e.target.value,
-            content,
-            created_at: new Date(),
-          });
-        }}
-        placeholder="Untitled"
-        className="text-2xl font-bold bg-transparent border-none outline-none placeholder:text-gray-400 focus:ring-0"
-      />
+      <div className="flex items-center justify-between">
+        <input
+          type="text"
+          value={title}
+          onChange={(e) => {
+            setTitle(e.target.value);
+            updateNoteMutation.mutate({
+              id,
+              title: e.target.value,
+              content,
+              created_at: new Date(),
+            });
+          }}
+          placeholder="Untitled"
+          className="text-2xl font-bold bg-transparent border-none outline-none placeholder:text-gray-400 focus:ring-0"
+        />
+        <div className="text-sm text-muted-foreground flex items-center gap-1">
+          {updateNoteMutation.isPending ? (
+            "Saving..."
+          ) : (
+            <>
+              <Check size={14} className="text-green-500" />
+              <span>Saved</span>
+            </>
+          )}
+        </div>
+      </div>
       <div
         className="h-full cursor-text"
         onClick={() => editor?.commands.focus()}
