9bdd74cf4c0af8db6cf124f581b19c1dfd96c85a
paulbg <paulbg@hey.com>
2024-12-01 10:35:50 +0200

    chore: put all-notes to search

--- src/app/(dashboard)/search/page.tsx
+++ src/app/(dashboard)/search/page.tsx
@@ -1,5 +1,13 @@
 import Search from "@/modules/search/features/search";
+import AllNotes from "@/modules/search/features/all-notes";
 
 export default function SearchPage() {
-  return <Search />;
+  return (
+    <main>
+      <div className="min-h-[80vh] grid items-center">
+        <Search />
+      </div>
+      <AllNotes />
+    </main>
+  );
 }
