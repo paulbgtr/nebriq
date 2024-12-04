546f5783261e07d5ba2c734d48f7475e1bed9c76
paulbg <paulbg@hey.com>
2024-12-04 14:28:51 +0200

    feat: implement note suggestions

---
+++ src/shared/lib/tippy/suggestion.ts
@@ -0,0 +1,78 @@
+"use client";
+
+import { ReactRenderer } from "@tiptap/react";
+import tippy from "tippy.js";
+import { createClient } from "../supabase/client";
+
+import MentionList from "./mention-list";
+
+export default {
+  items: async ({ query }) => {
+    const supabase = await createClient();
+    const { data: notes } = await supabase.from("notes").select("*");
+
+    if (!notes) {
+      return [];
+    }
+
+    const notesTitles = notes.map((note) => note.title);
+
+    return notesTitles
+      .filter((item) => item.toLowerCase().startsWith(query.toLowerCase()))
+      .slice(0, 5);
+  },
+
+  render: () => {
+    let component;
+    let popup;
+
+    return {
+      onStart: (props) => {
+        component = new ReactRenderer(MentionList, {
+          props,
+          editor: props.editor,
+        });
+
+        if (!props.clientRect) {
+          return;
+        }
+
+        popup = tippy("body", {
+          getReferenceClientRect: props.clientRect,
+          appendTo: () => document.body,
+          content: component.element,
+          showOnCreate: true,
+          interactive: true,
+          trigger: "manual",
+          placement: "bottom-start",
+        });
+      },
+
+      onUpdate(props) {
+        component?.updateProps(props);
+
+        if (!props.clientRect) {
+          return;
+        }
+
+        popup?.[0].setProps({
+          getReferenceClientRect: props.clientRect,
+        });
+      },
+
+      onKeyDown(props) {
+        if (props.event.key === "Escape") {
+          popup?.[0].hide();
+          return true;
+        }
+
+        return component?.ref?.onKeyDown(props);
+      },
+
+      onExit() {
+        popup?.[0].destroy();
+        component?.destroy();
+      },
+    };
+  },
+};
