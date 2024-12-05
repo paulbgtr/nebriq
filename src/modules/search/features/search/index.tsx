c3a6aa09e6f26422509f4506b5330efa6efa5b06
paulbg <paulbg@hey.com>
2024-12-05 19:31:39 +0200

    chore: push to /search/[query] on search

--- src/modules/search/features/search/index.tsx
+++ src/modules/search/features/search/index.tsx
@@ -1,64 +1,40 @@
 "use client";
 
 import { useState } from "react";
-import { SearchBar } from "./searchbar";
-import { Note } from "@/types/note";
-import { searchUsingTFIDF } from "@/app/actions/search/tfidf";
-import { useNotes } from "@/hooks/use-notes";
-import { NoteList } from "@/shared/components/note-list";
-import { convertTFIDFToNotesWithDefaults } from "@/shared/lib/utils";
+import SearchBar from "@/shared/components/search-bar";
+import { useRouter } from "next/navigation";
 
 export default function Search() {
   const [searchQuery, setSearchQuery] = useState("");
-  const [notes, setNotes] = useState<Note[]>([]);
-  const [hasSearched, setHasSearched] = useState(false);
 
-  const { getNotesQuery } = useNotes();
+  const router = useRouter();
 
   const handleSearch = async (e: React.FormEvent) => {
     e.preventDefault();
-    const notesData = getNotesQuery.data;
 
-    if (!notesData) return;
-
-    const results = await searchUsingTFIDF(searchQuery, notesData);
-
-    const convertedNotes = convertTFIDFToNotesWithDefaults(results);
-
-    setNotes(convertedNotes);
-
-    setHasSearched(true);
+    router.push(`/search/${encodeURIComponent(searchQuery)}`);
   };
 
   const handleSearchQueryChange = (newQuery: string) => {
     setSearchQuery(newQuery);
-    setHasSearched(false);
   };
 
   return (
-    <section className="mx-auto">
-      <SearchBar
-        searchQuery={searchQuery}
-        setSearchQuery={handleSearchQueryChange}
-        handleSearch={handleSearch}
-      />
-
-      {hasSearched && (
-        <div className="pt-8">
-          {notes.length > 0 ? (
-            <NoteList notes={notes} />
-          ) : (
-            <div className="text-center">
-              <h2 className="text-xl font-semibold text-gray-700">
-                No results found
-              </h2>
-              <p className="mt-2 text-gray-500">
-                Try adjusting your search terms or searching for something else
-              </p>
-            </div>
-          )}
+    <article className="grid h-full items-center">
+      <div
+        className={`transition-all duration-500 ease-in-out w-full max-w-md mx-auto`}
+      >
+        <div className="mb-6 text-center space-y-2">
+          <h1 className="text-xl font-semibold tracking-tight">
+            What&apos;s on your mind?
+          </h1>
         </div>
-      )}
-    </section>
+        <SearchBar
+          searchQuery={searchQuery}
+          setSearchQuery={handleSearchQueryChange}
+          handleSearch={handleSearch}
+        />
+      </div>
+    </article>
   );
 }
