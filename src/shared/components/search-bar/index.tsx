d2799cb10c4337c4ff907fc58bd35a3cfb0e8db5
paulbg <paulbg@hey.com>
2024-12-07 11:28:01 +0200

    feat: ai search to search bar and isAISearch state

--- src/shared/components/search-bar/index.tsx
+++ src/shared/components/search-bar/index.tsx
@@ -6,6 +6,7 @@ import { Input } from "@/shared/components/ui/input";
 import { Button } from "@/shared/components/ui/button";
 import { cn } from "@/shared/lib/utils";
 import { FaArrowUp } from "react-icons/fa";
+import { AISearch } from "./ai-search";
 
 type SearchBarProps = {
   searchQuery: string;
@@ -19,9 +20,10 @@ export default function SearchBar({
   handleSearch,
 }: SearchBarProps) {
   const [isFocused, setIsFocused] = useState(false);
+  const [isAISearch, setIsAISearch] = useState(false);
 
   return (
-    <>
+    <article className="flex flex-col gap-2 justify-end rounded-md p-2 bg-accent pb-3">
       <form onSubmit={handleSearch}>
         <div
           className={cn(
@@ -43,29 +45,32 @@ export default function SearchBar({
               onChange={(e) => setSearchQuery(e.target.value)}
               onFocus={() => setIsFocused(true)}
               onBlur={() => setIsFocused(false)}
-              placeholder="Type to search..."
+              placeholder="Seek and you shall find..."
               className={cn(
                 "pl-10 pr-24 h-12 transition-all duration-300",
                 isFocused && "ring-2 ring-offset-2",
                 "placeholder:text-muted-foreground/60"
               )}
             />
-            <Button
-              type="submit"
-              variant="secondary"
-              size="sm"
-              disabled={searchQuery.length <= 0}
-              className={cn(
-                "absolute right-2 hover:bg-muted transition-all duration-300 rounded-full w-8 h-8",
-                searchQuery &&
-                  "bg-primary text-primary-foreground hover:bg-primary/90"
-              )}
-            >
-              <FaArrowUp className="h-4 w-4" />
-            </Button>
+            <div className="absolute right-2 flex items-center space-x-2">
+              <Button
+                type="submit"
+                variant="secondary"
+                size="sm"
+                disabled={searchQuery.length <= 0}
+                className={cn(
+                  "hover:bg-muted transition-all duration-300 rounded-full w-8 h-8",
+                  searchQuery &&
+                    "bg-primary text-primary-foreground hover:bg-primary/90"
+                )}
+              >
+                <FaArrowUp className="h-4 w-4" />
+              </Button>
+            </div>
           </div>
         </div>
       </form>
-    </>
+      <AISearch isAISearch={isAISearch} setIsAISearch={setIsAISearch} />
+    </article>
   );
 }
