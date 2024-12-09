67c7301afb109a751c6352dca31351b3ac114b52
paulbg <paulbg@hey.com>
2024-12-09 09:22:39 +0200

    feat: represent llm answer with related notes

--- src/modules/search/features/query/results.tsx
+++ src/modules/search/features/query/results.tsx
@@ -3,35 +3,50 @@ import { Spinner } from "@/shared/components/spinner";
 import { Note } from "@/types/note";
 import { useTypewriter } from "@/hooks/use-typewriter";
 import { motion } from "framer-motion";
+import { LLMAnswer } from "@/types/llm-answer";
 
 type ResultsProps = {
-  answer: Note[] | string;
+  answer: Note[] | LLMAnswer;
   hasSearched: boolean;
 };
 
 export const Results = ({ answer, hasSearched }: ResultsProps) => {
-  const { displayedText } =
-    typeof answer === "string" ? useTypewriter(answer) : { displayedText: "" };
+  const displayedText = useTypewriter(
+    (answer as LLMAnswer)?.answer ?? "",
+    30
+  ).displayedText;
 
   return (
     <div className="flex flex-col gap-4">
       <div className="flex items-center justify-center">
         {hasSearched ? (
           <div className="w-full">
-            {typeof answer === "string" ? (
-              <div className="prose prose-xl max-w-3xl mx-auto flex justify-center">
-                <motion.p
-                  className="whitespace-pre-wrap"
-                  initial={{ opacity: 0 }}
-                  animate={{ opacity: 1 }}
-                  transition={{ duration: 0.3 }}
-                >
-                  {displayedText}
-                </motion.p>
+            {!Array.isArray(answer) ? (
+              <div className="max-w-3xl mx-auto">
+                {(answer as LLMAnswer).notes.length > 0 && (
+                  <div className="flex flex-col gap-2">
+                    <h3 className="text-lg font-semibold text-gray-700">
+                      Related notes
+                    </h3>
+                    <NoteList
+                      notes={(answer as LLMAnswer).notes.map((note) => note)}
+                    />
+                  </div>
+                )}
+                <div className="prose prose-xl flex flex-col gap-4">
+                  <motion.p
+                    className="whitespace-pre-wrap"
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 1 }}
+                    transition={{ duration: 0.3 }}
+                  >
+                    {displayedText}
+                  </motion.p>
+                </div>
               </div>
-            ) : Array.isArray(answer) && answer.length > 0 ? (
+            ) : answer.length > 0 ? (
               <div className="max-w-4xl mx-auto">
-                <NoteList notes={answer as any} />
+                <NoteList notes={answer as Note[]} />
               </div>
             ) : (
               <div className="text-center p-8 rounded-lg">
