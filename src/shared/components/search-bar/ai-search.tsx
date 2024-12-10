75ac8199c079f6336ed4537b72423d52f2220282
paulbg <paulbg@hey.com>
2024-12-10 17:18:26 +0200

    chore: ai-search toggle redesign
    - icon instead of text
    - tooltip explanation

--- src/shared/components/search-bar/ai-search.tsx
+++ src/shared/components/search-bar/ai-search.tsx
@@ -1,18 +1,48 @@
-import { Label } from "../ui/label";
 import { Switch } from "../ui/switch";
 import { useSearchStore } from "@/store/search";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "../ui/tooltip";
+import { Sparkles } from "lucide-react";
+import { cn } from "@/shared/lib/utils";
+import { Label } from "../ui/label";
 
 export const AISearch = () => {
   const { isAiSearch, setIsAiSearch } = useSearchStore();
 
   return (
-    <div className="flex items-center space-x-2 self-end">
-      <Switch
-        id="ai-search"
-        checked={isAiSearch}
-        onCheckedChange={setIsAiSearch}
-      />
-      <Label htmlFor="ai-search">AI Search</Label>
-    </div>
+    <TooltipProvider>
+      <div className="flex items-center space-x-2 self-end">
+        <Tooltip>
+          <TooltipTrigger asChild>
+            <div className="flex items-center space-x-2 cursor-pointer">
+              <Switch
+                id="ai-search"
+                checked={isAiSearch}
+                onCheckedChange={setIsAiSearch}
+                className={cn("data-[state=checked]:bg-primary")}
+              />
+              <Label
+                htmlFor="ai-search"
+                className={cn(
+                  "flex items-center space-x-1 cursor-pointer select-none"
+                )}
+              >
+                <Sparkles className="w-4 h-4" />
+              </Label>
+            </div>
+          </TooltipTrigger>
+          <TooltipContent>
+            <p className="max-w-sm">
+              Enable AI-powered semantic search for more accurate results and
+              summary
+            </p>
+          </TooltipContent>
+        </Tooltip>
+      </div>
+    </TooltipProvider>
   );
 };
