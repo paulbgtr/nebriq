f0ef578ce61adcae19b4456c7ae148d4338279e4
paulbg <paulbg@hey.com>
2024-12-10 17:19:03 +0200

    chore: remove padding from note-list

--- src/shared/components/note-list.tsx
+++ src/shared/components/note-list.tsx
@@ -37,7 +37,7 @@ export const NoteList = ({ notes }: NoteListProps) => {
 
   return (
     <motion.div
-      className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 p-4"
+      className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4"
       variants={container}
       initial="hidden"
       animate="show"
