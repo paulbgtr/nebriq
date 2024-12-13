0daf219cff8dec69dcf3be8288af6352c168c16a
paulbg <paulbg@hey.com>
2024-12-13 12:43:46 +0200

    chore: implement use-user with react-query

--- src/hooks/use-user.ts
+++ src/hooks/use-user.ts
@@ -1,29 +1,25 @@
-import { useState, useEffect } from "react";
 import { User } from "@/types/user";
 import { createClient } from "@/shared/lib/supabase/client";
+import { useQuery } from "@tanstack/react-query";
 
 export const useUser = () => {
-  const [user, setUser] = useState<User | undefined>();
-
-  useEffect(() => {
-    const getUserData = async () => {
+  const { data: user, isPending } = useQuery({
+    queryKey: ["user"],
+    queryFn: () => {
       const supabase = createClient();
-      const { data } = await supabase.auth.getUser();
-
-      if (!data?.user) return;
-
+      return supabase.auth.getUser();
+    },
+    select: ({ data }) => {
+      if (!data?.user) return null;
       const userData = {
         id: data?.user?.id,
         email: data?.user?.email,
         role: data?.user?.role,
         createdAt: new Date(data?.user?.created_at),
       } satisfies User;
+      return userData;
+    },
+  });
 
-      setUser(userData);
-    };
-
-    getUserData();
-  }, [user]);
-
-  return { user, setUser };
+  return { user, isPending };
 };
