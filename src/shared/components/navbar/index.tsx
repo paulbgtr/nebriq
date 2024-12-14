b55ec66d403e3469035ffb4ae8ec6cee781e8c47
paulbg <paulbg@hey.com>
2024-12-14 11:58:38 +0200

    chore: remove dummy note data from navbar

--- src/shared/components/navbar/index.tsx
+++ src/shared/components/navbar/index.tsx
@@ -6,22 +6,7 @@ import { NoteTabs } from "./note-tabs";
 import { Navigation } from "./navigation";
 
 export default function Navbar() {
-  const { user, isPending } = useUser();
-
-  // todo: get note ids from local-storage
-
-  const notes = [
-    {
-      id: "11",
-      user_id: "user456",
-      title: "Python for Data Science",
-      content:
-        "Python is a popular language for data analysis, machine learning, and visualization. Key libraries include NumPy, Pandas, and Matplotlib.",
-      tags: ["Python", "Data", "Science"],
-      linked_notes: [],
-      created_at: new Date("2024-01-11T20:00:00Z"),
-    },
-  ];
+  const { user } = useUser();
 
   return (
     <nav>
