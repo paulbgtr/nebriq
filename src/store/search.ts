6075118065271ad553df3fb480274e2767361dab
paulbg <paulbg@hey.com>
2024-12-15 21:20:02 +0200

    chore: store ai-search state

--- src/store/search.ts
+++ src/store/search.ts
@@ -1,11 +1,20 @@
 import { create } from "zustand";
+import { persist, createJSONStorage } from "zustand/middleware";
 
 interface SearchStore {
   isAiSearch: boolean;
   setIsAiSearch: () => void;
 }
 
-export const useSearchStore = create<SearchStore>()((set) => ({
-  isAiSearch: false,
-  setIsAiSearch: () => set((state) => ({ isAiSearch: !state.isAiSearch })),
-}));
+export const useSearchStore = create<SearchStore>()(
+  persist(
+    (set) => ({
+      isAiSearch: false,
+      setIsAiSearch: () => set((state) => ({ isAiSearch: !state.isAiSearch })),
+    }),
+    {
+      name: "search-storage",
+      storage: createJSONStorage(() => localStorage),
+    }
+  )
+);
