d05a63008232a7822d4cacff2bffaab26f158df4
paulbg <paulbg@hey.com>
2024-12-15 21:24:20 +0200

    fix: put placeholders for empty title / content

--- src/app/actions/search/semantic-search.ts
+++ src/app/actions/search/semantic-search.ts
@@ -4,9 +4,9 @@ import { OpenAIEmbeddings } from "@langchain/openai";
 import { MemoryVectorStore } from "langchain/vectorstores/memory";
 import { Note } from "@/types/note";
 
-// if (!process.env.OPENAI_API_KEY) {
-//   throw new Error("OPENAI_API_KEY is not defined in environment");
-// }
+if (!process.env.OPENAI_API_KEY) {
+  throw new Error("OPENAI_API_KEY is not defined in environment");
+}
 
 const embeddings = new OpenAIEmbeddings({
   apiKey: process.env.OPENAI_API_KEY,
@@ -42,8 +42,9 @@ const convertDocumentToNote = (document: Document) => {
   return {
     id: document.metadata.id,
     user_id: document.metadata.user_id,
-    title: document.metadata.title,
-    content: document.pageContent,
+    title:
+      document.metadata.title === "no title" ? "" : document.metadata.title,
+    content: document.pageContent === "no content" ? "" : document.pageContent,
     tags: document.metadata.tags,
     created_at: document.metadata.created_at,
   } satisfies Note;
@@ -55,11 +56,11 @@ export const semanticSearch = async (
 ): Promise<Note[]> => {
   const documents = notes.map((note) => {
     return {
-      pageContent: note.content ?? "",
+      pageContent: note.content || "no content",
       metadata: {
         id: note.id,
         user_id: note.user_id,
-        title: note.title ?? "",
+        title: note.title || "no title",
         tags: note.tags ?? [],
         created_at: note.created_at,
       },
