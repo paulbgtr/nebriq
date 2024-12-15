49cb251a7d6faca226eb650942ec6d6d498562f4
paulbg <paulbg@hey.com>
2024-12-15 21:13:43 +0200

    feat: note-tabs management using store

--- src/shared/components/navbar/note-tabs.tsx
+++ src/shared/components/navbar/note-tabs.tsx
@@ -1,38 +1,42 @@
-import { useNoteTabs } from "@/hooks/use-note-tabs";
-import { useSearchParams } from "next/navigation";
 import { X } from "lucide-react";
 import { Tabs, TabsList } from "@/shared/components/ui/tabs";
 import Link from "next/link";
 import { cn } from "@/shared/lib/utils";
+import { useNoteTabsStore } from "@/store/note-tabs";
+import { useSearchParams } from "next/navigation";
+import { formatHTMLNoteContent } from "@/shared/lib/utils";
 
 export const NoteTabs = () => {
-  const { getTabsQuery, deleteNoteTabMutation } = useNoteTabs();
   const searchParams = useSearchParams();
   const currentNoteId = searchParams.get("id");
 
-  const notesTabs = getTabsQuery.data ?? [];
+  const { openNotes, setOpenNotes } = useNoteTabsStore();
 
   return (
     <Tabs className="w-full h-full" orientation="vertical">
       <TabsList className="h-full flex bg-background overflow-y-auto">
-        {notesTabs.map((noteTab) => (
+        {openNotes.map((note) => (
           <Link
-            key={noteTab.id}
-            href={`/write?id=${noteTab.notes.id}`}
+            key={note.id}
+            href={`/write?id=${note.id}`}
             className={cn(
               "group flex items-center justify-between w-full px-3 py-2 relative hover:bg-accent/50 rounded-sm transition-colors",
-              noteTab.notes.id === currentNoteId && "bg-accent"
+              note.id === currentNoteId && "bg-accent"
             )}
           >
             <div className="text-sm font-medium truncate min-w-0 flex-1">
-              {noteTab.notes.title}
+              {note.title
+                ? note.content
+                  ? `${note.title} - ${formatHTMLNoteContent(note.content)}`
+                  : note.title
+                : "Untitled"}
             </div>
             <button
               className="ml-2 rounded-sm opacity-0 group-hover:opacity-100 hover:bg-accent p-0.5 transition-opacity flex-shrink-0"
               onClick={(e) => {
                 e.preventDefault();
 
-                deleteNoteTabMutation.mutate(noteTab.id);
+                setOpenNotes(openNotes.filter((n) => n.id !== note.id));
               }}
             >
               <X className="h-3 w-3" />
