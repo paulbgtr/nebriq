48cf7267aba51360dabd8ffc1c0d1e3b6ed8d137
paulbg <paulbg@hey.com>
2024-12-17 11:37:58 +0200

    chore: use controls and visualization

--- src/modules/graph/index.tsx
+++ src/modules/graph/index.tsx
@@ -1,13 +1,18 @@
+"use client";
+
+import React, { useRef } from "react";
 import { Visualization } from "./features/visualization";
 import { Controls } from "@/modules/graph/features/controls";
 import { Sidebar } from "./features/sidebar";
 
 export default function GraphModule() {
+  const svgRef = useRef<SVGSVGElement>(null);
+
   return (
     <div className="flex h-[calc(100vh-11rem)] w-full">
       <div className="relative flex-1 bg-background/95">
-        <Visualization />
-        <Controls />
+        <Visualization svgRef={svgRef} />
+        <Controls svgRef={svgRef} />
       </div>
 
       <Sidebar />
