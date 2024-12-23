fc2eb91739c72255daeadab985af19e890b6d4f9
paulbg <paulbg@hey.com>
2024-12-23 16:30:38 +0200

    chore: remove unnecessary types
    tag, token-limits, note-connection

--- src/types/note-connection.ts
+++
@@ -1,9 +0,0 @@
-export type NoteConnection = {
-  id: string;
-  user_id?: string;
-  note_id_from: string;
-  note_id_to: string;
-  created_at?: Date;
-};
-
-export type CreateNoteConnection = Omit<NoteConnection, "id">;
