53b121b33ff202e7ad02b92d874a8dc1f92eefb3
paulbg <paulbg@hey.com>
2024-12-24 15:19:53 +0200

    feat: make search container more responsive

--- src/app/(dashboard)/search/page.tsx
+++ src/app/(dashboard)/search/page.tsx
@@ -3,12 +3,15 @@ import AllNotes from "@/modules/search/features/all-notes";
 
 export default function SearchPage() {
   return (
-    <main className="min-h-[calc(100vh-12rem)] flex flex-col items-center justify-center">
-      <div className="my-auto w-full">
-        <Search />
-      </div>
-      <div className="mt-16 min-h-[450px]">
-        <AllNotes />
+    <main className="min-h-[calc(100vh-12rem)] flex flex-col items-center justify-start px-4 sm:px-6 lg:px-8">
+      <div className="w-full max-w-7xl mx-auto pt-8 sm:pt-12 lg:pt-16">
+        <div className="w-full">
+          <Search />
+        </div>
+
+        <div className="mt-8 sm:mt-12 lg:mt-16 min-h-[450px] w-full">
+          <AllNotes />
+        </div>
       </div>
     </main>
   );
