cefffb768581842f53e1b73abefd21c904f0ce4b
paulbg <paulbg@hey.com>
2024-12-24 15:58:37 +0200

    feat: improve query ux/ui

--- src/modules/search/features/query/index.tsx
+++ src/modules/search/features/query/index.tsx
@@ -3,6 +3,7 @@
 import { Results } from "./results";
 import { useSearchQuery } from "@/hooks/use-search-query";
 import FollowUp from "./follow-up";
+import { motion } from "framer-motion";
 
 export default function SearchQuery() {
   const searchQueryResult = useSearchQuery();
@@ -12,13 +13,20 @@ export default function SearchQuery() {
   const { results, hasSearched } = searchQueryResult;
 
   return (
-    <div className="flex flex-col gap-4">
-      <Results results={results} hasSearched={hasSearched} />
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      className="relative flex flex-col gap-6 px-4 py-6 max-w-3xl mx-auto"
+    >
+      <div className="space-y-6">
+        <Results results={results} hasSearched={hasSearched} />
+      </div>
+
       {results.length > 0 && (
-        <div className="fixed bottom-5 left-0 right-0 max-w-xl mx-auto">
+        <div className="fixed bottom-5 left-0 right-0 px-4">
           <FollowUp relevantNotes={results} />
         </div>
       )}
-    </div>
+    </motion.div>
   );
 }
