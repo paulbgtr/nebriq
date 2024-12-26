6ccc144deb7cd653e3224ed7c0d515d6fab96301
paulbg <paulbg@hey.com>
2024-12-26 15:53:22 +0200

    fix(home): h -> min-h

--- src/app/(dashboard)/home/page.tsx
+++ src/app/(dashboard)/home/page.tsx
@@ -3,7 +3,7 @@ import AllNotes from "@/modules/home/features/all-notes";
 
 export default function HomePage() {
   return (
-    <main className="h-[calc(100vh-12rem)] flex flex-col items-center justify-start px-4 sm:px-6 lg:px-8">
+    <main className="min-h-[calc(100vh-12rem)] flex flex-col items-center justify-start px-4 sm:px-6 lg:px-8">
       <div className="w-full max-w-7xl mx-auto pt-8 sm:pt-12 lg:pt-16">
         <div className="w-full">
           <Search />
