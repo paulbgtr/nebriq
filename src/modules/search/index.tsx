8b9e1fc71daad8a1668f11a4b0c15e39a61c82d6
paulbg <paulbg@hey.com>
2024-12-30 16:12:16 +0200

    chore: improve query page layout
    - set max width to 7xl
    - remove unused motion opacity transition

--- src/modules/search/index.tsx
+++ src/modules/search/index.tsx
@@ -2,7 +2,6 @@
 
 import { Results } from "./components/results";
 import { useSearchQuery } from "@/hooks/use-search-query";
-import { motion } from "framer-motion";
 
 export default function SearchQuery() {
   const searchQueryResult = useSearchQuery();
@@ -12,14 +11,10 @@ export default function SearchQuery() {
   const { results, hasSearched } = searchQueryResult;
 
   return (
-    <motion.div
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      className="relative flex flex-col gap-6 px-4 py-6 max-w-3xl mx-auto"
-    >
+    <div className="relative flex flex-col gap-6 px-4 py-6 max-w-7xl mx-auto">
       <div className="space-y-6">
         <Results results={results} hasSearched={hasSearched} />
       </div>
-    </motion.div>
+    </div>
   );
 }
