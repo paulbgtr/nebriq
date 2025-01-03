3476477bba423fe73100c6ea5625660e531d2a65
paulbg <paulbg@hey.com>
2025-01-03 12:07:17 +0200

    feat: theme provider

--- src/providers.tsx
+++ src/providers.tsx
@@ -1,10 +1,20 @@
 "use client";
 
 import { QueryClientProvider } from "@tanstack/react-query";
+import { ThemeProvider } from "./shared/lib/next-themes/theme-provider";
 import queryClient from "./shared/lib/react-query";
 
 export default function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider
+        attribute="class"
+        defaultTheme="system"
+        enableSystem
+        disableTransitionOnChange
+      >
+        {children}
+      </ThemeProvider>
+    </QueryClientProvider>
   );
 }
