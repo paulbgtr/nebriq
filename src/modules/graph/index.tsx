c78d0c3385a5edfd50abeb46914247877ed1b8a8
paulbg <paulbg@hey.com>
2025-01-03 14:44:35 +0200

    chore: dump sidebar and controls from graph
    not using this atm.

--- src/modules/graph/index.tsx
+++ src/modules/graph/index.tsx
@@ -1,21 +1,13 @@
 "use client";
 
-import React, { useRef } from "react";
 import { Visualization } from "./features/visualization";
-import { Controls } from "@/modules/graph/features/controls";
-// import { Sidebar } from "./features/sidebar";
 
 export default function GraphModule() {
-  const svgRef = useRef<SVGSVGElement>(null);
-
   return (
     <div className="flex h-[calc(100vh-11rem)] w-full">
       <div className="relative flex-1 bg-background/95">
-        <Visualization svgRef={svgRef} />
-        <Controls svgRef={svgRef} />
+        <Visualization />
       </div>
-
-      {/* <Sidebar /> */}
     </div>
   );
 }
