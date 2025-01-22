49ffdc18e30448198742fbd5f07035b568d10042
paulbg <paulbg@hey.com>
2025-01-22 12:49:16 +0200

    chore(settings/layout): responsive design

--- src/app/(dashboard)/(user)/settings/layout.tsx
+++ src/app/(dashboard)/(user)/settings/layout.tsx
@@ -4,6 +4,8 @@ export default async function SettingsLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="container max-w-4xl mx-auto py-8 space-y-8">{children}</div>
+    <div className="container mx-auto px-4 sm:px-6 lg:px-8 py-4 sm:py-6 lg:py-8 space-y-4 sm:space-y-6 lg:space-y-8 max-w-4xl">
+      {children}
+    </div>
   );
 }
