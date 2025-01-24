9a5fa6f07536453596382fe8cd01d158f0605fed
paulbg <paulbg@hey.com>
2025-01-24 12:54:37 +0200

    chore(note-list): items center

--- src/shared/components/note-list.tsx
+++ src/shared/components/note-list.tsx
@@ -55,7 +55,7 @@ export const NoteList = ({
 
   return (
     <motion.div
-      className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4"
+      className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 items-center"
       variants={container}
       initial="hidden"
       animate="show"
