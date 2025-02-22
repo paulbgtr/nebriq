229604d7145fc03ca535187ac63872f2acae5ae6
paulbg <paulbg@hey.com>
2025-02-22 10:38:24 +0200

    chore(graph): add rounded-lg to graph container

--- src/modules/graph/index.tsx
+++ src/modules/graph/index.tsx
@@ -5,7 +5,7 @@ import { Visualization } from "./features/visualization";
 export default function GraphModule() {
   return (
     <div className="flex h-[calc(100vh-11rem)] w-full">
-      <div className="relative flex-1 bg-background/95">
+      <div className="relative flex-1 bg-background/95 rounded-lg">
         <Visualization />
       </div>
     </div>
