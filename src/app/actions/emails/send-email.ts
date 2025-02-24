257ca567cad507dee285cee3e4e12c0c0e1abf68
paulbg <paulbg@hey.com>
2025-02-24 13:33:26 +0200

    feat(send-email): generic type, template support

--- src/app/actions/emails/send-email.ts
+++ src/app/actions/emails/send-email.ts
@@ -1,27 +1,32 @@
 "use server";
 
 import { client } from "@/shared/lib/resend/client";
+import { getEmailTemplate, EmailTemplate, EmailData } from "@/shared/lib/utils";
 
-export const sendEmail = async (
+export const sendEmail = async <T extends EmailTemplate>(
   subject: string,
   emailFrom: string,
   emailTo: string,
-  template: React.ReactNode
+  template: T,
+  data: EmailData[T]
 ) => {
   try {
-    const { data, error } = await client.emails.send({
+    const emailComponent = getEmailTemplate(template, data);
+
+    const { data: responseData, error } = await client.emails.send({
       from: emailFrom,
       to: [emailTo],
-      subject,
-      react: template,
+      subject: subject,
+      react: emailComponent,
     });
 
     if (error) {
       throw error;
     }
 
-    return data;
+    return responseData;
   } catch (error) {
+    console.error("Error sending email:", error);
     throw error;
   }
 };
