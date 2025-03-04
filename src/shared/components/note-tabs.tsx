983b7dec4d9062cd6ebbb1bc72bb0366a750213c
paulbg <paulbg@hey.com>
2025-03-04 17:21:02 +0200

    chore: move user-actions, note-tabs to shared

--- src/shared/components/note-tabs.tsx
+++ src/shared/components/note-tabs.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { X } from "lucide-react";
 import { cn } from "@/shared/lib/utils";
 import { useNoteTabsStore } from "@/store/note-tabs";
--- src/shared/components/user-actions.tsx
+++ src/shared/components/user-actions.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { useState } from "react";
 import { Button } from "@/shared/components/ui/button";
 import {
