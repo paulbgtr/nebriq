0366efb09a26a128664e3ad7cf470aaa0e783dda
paulbg <paulbg@hey.com>
2025-03-04 17:26:12 +0200

    chore(graph): layout tweaks

--- src/modules/graph/index.tsx
+++ src/modules/graph/index.tsx
@@ -4,10 +4,8 @@ import { Visualization } from "./features/visualization";
 
 export default function GraphModule() {
   return (
-    <div className="flex h-[calc(100vh-11rem)] w-full">
-      <div className="relative flex-1 bg-background/95 rounded-lg">
-        <Visualization />
-      </div>
+    <div className="w-full h-[calc(100vh-8rem)] flex items-center justify-center">
+      <Visualization />
     </div>
   );
 }
