dd4d18614ba2b3a23027507a9c212e1d28b7c0fd
paulbg <paulbg@hey.com>
2025-03-04 17:21:57 +0200

    feat(/layout/dashboard): rethink the layout
    - use sidebar instead of navbar
    - use note-tabs separately
    - use user-actions separately

--- src/app/(dashboard)/home/page.tsx
+++ src/app/(dashboard)/home/page.tsx
@@ -1,26 +1,20 @@
 "use client";
 
-import Search from "@/modules/home/features/search";
-import AllNotes from "@/modules/home/features/all-notes";
-import { useUser } from "@/hooks/use-user";
+import { useUser } from "@/shared/hooks/use-user";
 import { Onboarding } from "@/shared/components/onboarding";
+import HomeModule from "@/modules/home";
 
 export default function HomePage() {
   const { isNewUser } = useUser();
 
   return (
-    <main className="min-h-[calc(100vh-12rem)] flex flex-col items-center justify-start px-4 sm:px-6 lg:px-8">
-      {isNewUser && <Onboarding />}
-
-      <div className="w-full max-w-5xl mx-auto pt-8 sm:pt-12 lg:pt-16">
-        <div className="w-full max-w-2xl mx-auto">
-          <Search />
+    <main>
+      {isNewUser && (
+        <div className="w-full max-w-5xl mx-auto px-4 sm:px-6 lg:px-8 py-8 shrink-0">
+          <Onboarding />
         </div>
-
-        <div className="mt-12 sm:mt-16 lg:mt-20 min-h-[450px] w-full">
-          <AllNotes />
-        </div>
-      </div>
+      )}
+      <HomeModule />
     </main>
   );
 }
